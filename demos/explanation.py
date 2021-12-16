3dca692de3b4178683b3e05b436a963175c13790
JasonMorrisSC <jason.morris@servicecanada.gc.ca>
2021-12-16 13:56:55 -0700

    Correct bugs in explanation.py Fixes #19

--- demos/explanation.py
+++ demos/explanation.py
@@ -2,6 +2,8 @@ import requests
 import json
 from pprint import pprint
 import networkx as nx
+from networkx import descendants, all_simple_paths
+from numpy import unique
 
 # From inside the root of this repository, run `pip install .` followed by
 # `openfisca serve` to run the WebAPI locally.
@@ -138,6 +140,8 @@ for (K,V) in response['trace'].items():
         related_node = K.replace("_known<","<")
         dependency_graph.nodes[related_node]['known'] = V['value']
 
+from pprint import pprint
+
 
 # The above creates a network of nodes, each of which has a 'value' and a 'known' attribute,
 # and edges from each variable to the variables on which it depended.
@@ -145,14 +149,30 @@ for (K,V) in response['trace'].items():
 # Now we can go through all the nodes in the graph and add a third value called 'display'
 # that will include the value if it is known, and the string "unknown" otherwise.
 
-for N in dependency_graph:
-    values = dependency_graph.nodes[N]['value']
-    knowns = dependency_graph.nodes[N]['known']
-    display = []
-    for (value, known) in zip(values,knowns):
-        display.append(value if known else "unknown")
-    dependency_graph.nodes[N]['display'] = display
+# Note that there is something about the above algorithm that generates
+# duplicate nodes with no attributes, so we use the incorrect_nodes list
+# below to remove them, which seems to solve the problem.
 
+incorrect_nodes = []
+for N in dependency_graph:
+    # Not sure why this test is necessary, but duplicate nodes
+    # with no value show up for root elements in the graph
+    if 'value' in dependency_graph.nodes[N]:
+      values = dependency_graph.nodes[N]['value']
+      if 'known' in dependency_graph.nodes[N]:
+        knowns = dependency_graph.nodes[N]['known']
+      else: # Some output variables do not have associated "known" variables.
+        knowns = [True] * len(values)
+        dependency_graph.nodes[N]['known'] = knowns
+      display = []
+      for (value, known) in zip(values,knowns):
+          display.append(value if known else "unknown, potentially " + str(value))
+      dependency_graph.nodes[N]['display'] = display
+    else:
+      incorrect_nodes.append(N)
+
+for i in incorrect_nodes:
+  dependency_graph.remove_node(i)
 
 ## Now we can use the graph to do complicated things. First, we use it to generate a
 # natural langauge explanation for how each conclusion requested was reached.
@@ -161,6 +181,7 @@ for N in dependency_graph:
 # from the graph given a starting node, and the index of the entity that the explanation
 # is for. Remember that all "values" in the data are actually a list of values, one
 # for each entity included.
+
 
 def generate_explanation(goal,entity,index=0):
     indent = ' ' * index
@@ -172,53 +193,66 @@ def generate_explanation(goal,entity,index=0):
         output += generate_explanation(neighbour,entity,index)
     return output
 
-# Now we find the name of the variables we were actually looking for,
-# eliminate the "known" variables, and call our function for each 
-# entity for each variable.
 
-from networkx import ancestors, descendants
-from numpy import unique
+#pprint(adjacency_data(dependency_graph))
+
+# Now we find the name of the variables we were actually looking for,
+# eliminate the "known" variables, and process the results for each
+
+
+from networkx import all_simple_paths
 
 for goal in unique(response['requestedCalculations']):
     if "_known<" not in goal:
+        print("--------------------------------------")
         i = 0
         while i < len(dependency_graph.nodes[goal]['value']):
             print("For Entity #" + str(i+1) + ":")
-            explanation = generate_explanation(goal,i)[:-6] # The slice removes the trailing ", and\n"
             
-            print(explanation)
-            # We can also use the graph to determine what input variables remain relevant.
+            # We can use the graph to determine what input variables remain relevant.
             # An input variable is a variable with no descendents in the dependency graph.
             # It is relevant if it is not descendent of a "known" variable.
+
             relevant = []
-            if dependency_graph.nodes[goal]['display'][i] == "unknown":    
-                for node in descendants(dependency_graph,goal):
-                    # print("Checking " + str(node))
-                    # print(str(descendants(dependency_graph,node)))
-                    # print(str(ancestors(dependency_graph,node)))
-                    if not descendants(dependency_graph,node): # Only leaf nodes
-                        known_parent = False
-                        for parent in ancestors(dependency_graph,node):
-                            if dependency_graph.nodes[parent]['display'][i] != "unknown":
+
+            for node in descendants(dependency_graph,goal):
+                if not descendants(dependency_graph,node): # Only leaf nodes
+                    if not dependency_graph.nodes[node]['known'][i]:
+                      known_parent = False
+                      for path in all_simple_paths(dependency_graph,goal,node):
+                        for parent in path:
+                            if dependency_graph.nodes[parent]['known'][i] == True:
                                 known_parent = True
+                                break
                         if not known_parent:
                             relevant.append(node)
-                print("The remaining relevant inputs are " + str(relevant))
-            else:
-                print("The goal is known, there are no relevant variables.")
+
             # If we know which input variables will not be collected by the interface, and the
             # list of relevant variables is exclusively from that list, we can infer that the
             # conclusion is contingent on things we won't collect, and advise the user
-            # accordingly.
+            # accordingly. Note that you will need to generate the list
+            # with the same period used in the inputs.
+            
             unaskable = ['eligible_under_social_agreement<2021-12-01>']
+            conditional = False
             relevant_and_askable = False
-            for q in relevant:
+            if relevant:
+              for q in relevant:
                 if q not in unaskable:
                     relevant_and_askable = True
-            if not relevant_and_askable and dependency_graph.nodes[goal]['display'][i] == "unknown":
-                print("There are no askable relevant variables, so this goal is contingently known.")
+              if not relevant_and_askable and dependency_graph.nodes[goal]['known'][i] == False:
+                conditional = True
+                dependency_graph.nodes[goal]['display'][i] = "conditionally known, potentially " + str(dependency_graph.nodes[goal]['value'][i])
+
+            # Now we can generate an explanation with the changes above,
+            # and display it to the user along with the other useful information.
+                
+            explanation = generate_explanation(goal,i)[:-6] # The slice removes the trailing ", and\n"
+            
+            print(explanation)
+            if conditional:
+              print("The conclusion is conditionally known, because all remaining relevant inputs are not askable.")
+            print("The remaining relevant inputs are " + str(unique(relevant)))
+            
+            
             i += 1
-
-
-
-
